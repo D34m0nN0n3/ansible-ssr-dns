ef37e0373752faca0f3b9e25be3e4911833b6f0b
Dmitriy Prigoda <46213676+D34m0nN0n3@users.noreply.github.com>
2021-12-03 00:02:01 +0300

    Update tabledynamic.js

--- docs/form/tabledynamic.js
+++ docs/form/tabledynamic.js
@@ -1,9 +1,10 @@
 function addRow() {
           
-    var Name = document.getElementById("name");
-    var IP1 = document.getElementById("ip1");
+    var NAME = document.getElementById("name");
+    var DOMAIN = document.getElementById("domain");
+    var IP = document.getElementById("ip");
  
-    let reversip = IP1.value;
+    let reversip = IP.value;
     let ptrip = reversip.split(".").reverse();
 
     var PTR = ptrip.join(".");
@@ -11,8 +12,8 @@ function addRow() {
     var rowCount = table.rows.length;
     var row = table.insertRow(rowCount);
  
-    row.insertCell(0).innerHTML= Name.value + '.' + DOMAIN.value + '.' + '&nbsp;&nbsp;&nbsp;&nbsp;IN&nbsp;&nbsp;A&nbsp;&nbsp;&nbsp;&nbsp;' + IP1.value;
-    row.insertCell(1).innerHTML= PTR + '.in-addr.arpa.&nbsp;&nbsp;&nbsp;&nbsp;IN&nbsp;&nbsp;PTR&nbsp;&nbsp;&nbsp;&nbsp;' + Name.value + '.' + DOMAIN.value + '.';
+    row.insertCell(0).innerHTML= NAME.value + '.' + DOMAIN.value + '.' + '&nbsp;&nbsp;&nbsp;&nbsp;IN&nbsp;&nbsp;A&nbsp;&nbsp;&nbsp;&nbsp;' + IP.value;
+    row.insertCell(1).innerHTML= PTR + '.in-addr.arpa.&nbsp;&nbsp;&nbsp;&nbsp;IN&nbsp;&nbsp;PTR&nbsp;&nbsp;&nbsp;&nbsp;' + NAME.value + '.' + DOMAIN.value + '.';
     row.insertCell(3).innerHTML= '<input type="button" value = "Удалить" onClick="Javacsript:deleteRow(this)">';
 
 }
